1c3fe054fcbaa16c3da78cc3087f212d47301f54
Kia Morelos <kmorelos@bti-usa.com>
2020-03-16 16:45:28 -0600

    add users and comments with foreign key

---
+++ db/migrate/20200316223526_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.text :description
+      t.references :user, index:true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
